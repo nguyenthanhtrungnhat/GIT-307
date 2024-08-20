314e340a39bd34b250b91dacb5a44f049765cd0c
nguyenthanhtrungnhat <167590830+nguyenthanhtrungnhat@users.noreply.github.com>
2024-08-20 21:53:50 +0700

    update

--- Lab/Lab10-NguyenThanhTrungNhat-2131200088/Assignment4.js
+++ Lab/Lab10-NguyenThanhTrungNhat-2131200088/Assignment4.js
@@ -2,6 +2,28 @@ $(document).ready(function () {
     let studentArray = new Array();
     let classArray = new Array();
     $.get(
+        'http://139.180.213.49/getdata-student.php',
+        {
+            Type: 'Student'
+        },
+        function (data) {
+            console.log(data)
+            data.sort((a, b) => {
+                return a.Class.localeCompare(b.Class);
+            })
+            let element = '';
+            let unique = '';
+            for (let each of data) {
+                if (each.Class != unique)
+                    element += `<li class='class-name' data-class='${each.Class}'>${each.Class}</li>`
+                unique = `${each.Class}`;
+            }
+
+            $('.myClass').append(element);
+        }, 'json'
+
+    )
+    $.get(
         'http://139.180.213.49/getdata-student.php', { Type: "Student" },
         function (data) {
             console.log(data);
@@ -17,8 +39,8 @@ $(document).ready(function () {
                                         <td>${i}</td>
                                         <td>${data[i].Name}</td>
                                         <td>${data[i].ID}</td>
-                                        <td><button>Edit</button>
-                                       <Button class="deleteid${data[i].ID}">Delete</Button></td>
+                                        <td><button class='edit' data-id='${data[i].ID}'>Edit</button>
+                                       <Button class="delete deleteid${data[i].ID}">Delete</Button></td>
                                     </tr>`;
                 $('.tableBody').append(element);
             }
@@ -26,7 +48,7 @@ $(document).ready(function () {
     )
 
     $(".add").click(function () {
-       
+
         let newID = $('.2230').val();
         let newName = $('.2231').val();
         let newClass = $('.2233').val();
@@ -54,15 +76,84 @@ $(document).ready(function () {
                                         <td>${i}</td>
                                         <td>${studentArray[i].Name}</td>
                                         <td>${studentArray[i].ID}</td>
-                                        <td><button>Edit</button>
-                                         <Button class="deleteid${studentArray[i].ID}">Delete</Button></td>
+                                        <td><button class='edit' data-id='${studentArray[i].ID}'>Edit</button>
+                                         <Button class="delete deleteid${studentArray[i].ID}">Delete</Button></td>
                                     </tr>`;
+            $('.tableBody').append(element);
+        }
+    });
+    $(document).on('click', '.delete', function () {
+
+        let idToDelete = $(this).attr('class').match(/deleteid(\d+)/)[1];
+
+        studentArray = studentArray.filter(student => student.ID !== idToDelete);
+
+        $('.tableBody').empty();
+
+        let element = "";
+        for (let i = 0; i < studentArray.length; i++) {
+            element = '';
+            element += `<tr>
+                            <td>${i}</td>
+                            <td>${studentArray[i].Name}</td>
+                            <td>${studentArray[i].ID}</td>
+                            <td><button class='edit' data-id='${studentArray[i].ID}'>Edit</button>
+                            <Button class="delete deleteid${studentArray[i].ID}">Delete</Button></td>
+                        </tr>`;
+            $('.tableBody').append(element);
+        }
+    });
+
+    $('#searchBtn').click(function () {
+        let searchName = $('.searchName').val().trim().toLowerCase();
+
+        if (searchName === '') {
+            alert('Please enter a name to search.');
+            return;
+        }
+
+        let filteredStudents = studentArray.filter(student =>
+            student.Name.toLowerCase().includes(searchName)
+        );
+
+        $('.tableBody').empty();
+
+        let element = "";
+        for (let i = 0; i < filteredStudents.length; i++) {
+            element = '';
+            element += `<tr>
+                                    <td>${i}</td>
+                                    <td>${filteredStudents[i].Name}</td>
+                                    <td>${filteredStudents[i].ID}</td>
+                                    <td><button class='edit' data-id='${filteredStudents[i].ID}'>Edit</button>
+                                    <Button class="delete deleteid${filteredStudents[i].ID}">Delete</Button></td>
+                                </tr>`;
             $('.tableBody').append(element);
         }
 
     });
 
+    $(document).on('click', '.class-name', function () {
+        let selectedClass = $(this).data('class');
+
+        let filteredStudents = studentArray.filter(student => student.Class === selectedClass);
+
+        $('.tableBody').empty();
 
 
+        let element = "";
+        for (let i = 0; i < filteredStudents.length; i++) {
+            element = '';
+            element += `<tr>
+                                <td>${i}</td>
+                                <td>${filteredStudents[i].Name}</td>
+                                <td>${filteredStudents[i].ID}</td>
+                                <td><button class='edit' data-id='${filteredStudents[i].ID}'>Edit</button>
+                                <Button class="delete deleteid${filteredStudents[i].ID}">Delete</Button></td>
+                            </tr>`;
+            $('.tableBody').append(element);
+        }
 
+
+    });
 });
